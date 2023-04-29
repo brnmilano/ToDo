75164800e5f65277a6a21807a25422eba53f8ad2
Bruno Milano <brn.milano@hotmail.com>
2023-04-28 23:50:15 -0300

    Adição da API fake e criação dos dados fakes

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,13 +1,60 @@
 import { Box } from '@mui/material';
 import styles from './styles.module.scss'
+import { Checkbox } from '../Checkbox';
+import trashIcon from '../../assets/trash.svg';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+interface TasksProps {
+  id: number,
+  description: string,
+}
 
 export function Tasks() {
+  const [tasks, setTasks] = useState<TasksProps[]>([]);
+
+  useEffect(() => {
+    api.get('/lista-de-tarefas')
+      .then(response => setTasks(response.data.tasks));
+  }, [])
+
+  console.log(tasks);
+
+
   return (
     <Box className={styles.tasksContainer}>
       <Box className={styles.tasksHeader}>
-        <p>Tarefas criadas</p>
+        <Box className={styles.tasksCreated}>
+          <span>
+            Tarefas criadas
 
-        <p>Concluídas</p>
+            <p>5</p>
+          </span>
+        </Box>
+
+        <Box className={styles.tasksCompleted}>
+          <span>
+            Concluídas
+
+            <p>0 de 5</p>
+          </span>
+        </Box>
+      </Box>
+
+      <Box>
+        <Box className={styles.tasksList}>
+          {tasks.map(task => (
+            <Box key={task.id} className={styles.tasks}>
+              <Checkbox />
+
+              <p>{task.description}</p>
+
+              <button>
+                <img src={trashIcon} alt="Apagar tarefa" />
+              </button>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import theme from './styles/theme';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createServer, Model } from 'miragejs';
 import { routes } from './routes/routes';
 import { ThemeProvider } from '@emotion/react';
 import './styles/index.scss';
+
+createServer({
+  models: {
+    task: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      tasks: [
+        {
+          id: 1,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod velit eget sapien bibendum, sit amet sollicitudin turpis efficitur. Sed at mollis felis. Donec cursus consectetur velit ac tristique.',
+        },
+        {
+          id: 2,
+          description: 'Duis efficitur eleifend mi, in dapibus sapien vestibulum sed. Proin convallis nulla ut lectus dapibus tristique.',
+        },
+        {
+          id: 3,
+          description: 'Mauris non elit non ex convallis bibendum sed in ipsum. Suspendisse rutrum, nisi non venenatis interdum, felis purus pellentesque elit, non fringilla risus augue nec leo. Duis sodales semper purus vel bibendum. Sed semper sapien sed purus porttitor, in faucibus odio venenatis. Nam at lobortis justo, nec venenatis metus.',
+        },
+        {
+          id: 4,
+          description: 'Nulla euismod, urna a lobortis varius, lacus mauris feugiat est, eget ultrices risus mauris nec nunc. Aenean imperdiet varius tortor ac iaculis. Sed a velit vel mi interdum aliquam. Sed euismod, magna sit amet dictum suscipit, massa sapien suscipit tortor, sed tincidunt sapien sapien eu nisl.',
+        },
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/lista-de-tarefas', () => {
+      return this.schema.all('task');
+    })
+
+    this.post('/lista-de-tarefas', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('task', data);
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
