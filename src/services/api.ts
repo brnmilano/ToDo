278a06e5172183a04ff17d29a4350253551e8ae1
Bruno Milano <brn.milano@hotmail.com>
2023-05-02 20:03:12 -0300

    Deploy do projeto

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://to-do-orcin-nu.vercel.app/lista-de-tarefas/api",
+  baseURL: "https://to-do-brnmilano.vercel.app/api",
 });
