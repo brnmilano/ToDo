97c70121f296a72a21a119f683299bca248cc29c
Bruno Milano <brn.milano@hotmail.com>
2023-05-03 15:19:31 -0300

    Deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://to-do-git-master-brnmilano.vercel.app/api",
+  baseURL: "https://to-do-git-master-brnmilano.vercel.app/lista-de-tarefas/api",
 });
